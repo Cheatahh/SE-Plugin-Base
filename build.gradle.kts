2e6fe3eec7affa8bf0af485bb900b83ffe2065f9
Cheatahh <72265408+Cheatahh@users.noreply.github.com>
2022-05-18 11:56:02 +0200

    Gradle Setup

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.6.21"
+}
+
+group = "cheatahh.jvm"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven {
+        setUrl("https://jitpack.io")
+    }
+}
+
+dependencies {
+    implementation("com.github.Vincent200355:AirportAgentSimulation-Base:master-SNAPSHOT")
+    testImplementation(kotlin("test"))
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
